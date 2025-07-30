cdf640f34209c7a5dd66a6966e2c619cc162fc60
Zdscsf <141560373+Zdscsf@users.noreply.github.com>
2025-07-30 14:00:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-require('dotenv').config(); // Charge les variables .env en local
+require('dotenv').config();
 
 const { Client, GatewayIntentBits } = require('discord.js');
 const express = require('express');
 const app = express();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
+const SALON_ARRIVEE_ID = '1400085345260802208';
+const SALON_DEPART_ID = '1400085379016691762';
 
 client.once('ready', () => {
   console.log('Bot est en ligne !');
@@ -16,9 +26,30 @@ client.on('messageCreate', message => {
   }
 });
 
+client.on('messageCreate', message => {
+  if (message.content === '!zd') {
+    message.channel.send('good!');
+  }
+});
+
+
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.cache.get(SALON_ARRIVEE_ID);
+  if (channel) {
+    channel.send(`Bienvenue ${member.user.username} 👋`);
+  }
+});
+
+client.on('guildMemberRemove', member => {
+  const channel = member.guild.channels.cache.get(SALON_DEPART_ID);
+  if (channel) {
+    channel.send(`${member.user.username} a quitté le serveur 😢`);
+  }
+});
+
 client.login(process.env.DISCORD_TOKEN);
 
-// Petit serveur express pour garder Render content (évite que Render coupe le process)
+// Serveur Web
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Bot Discord en ligne');
